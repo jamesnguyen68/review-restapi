1d3a8d2af99b2489c1bc51694ac6e665533b32d0
Quy Nguyen <nhonquy@gmail.com>
2021-04-28 06:34:06 -0700

    initial project

---
+++ src/test/java/com/dummy/rest/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dummy.rest.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
